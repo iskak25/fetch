1a0c6a98368a1591e5dd6b47e971cee8a4ddb725
iskak25 <iskak2512@gmail.com>
2022-10-18 00:20:03 +0600

    end commit

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
-let fech = document.querySelector(".fech");
+let addFetch = document.querySelector(".list");
+let btn = document.querySelector(".btn");
 let a;
+let backBtn = document.querySelector(".back_btn");
 const url = "https://jsonplaceholder.typicode.com/posts";
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    a = data.map((item) => {
-      return `<li>${item.id}</li>
-      <li>${item.title}</li>
-      <li>${item.body}</li>
-      `;
+function addText() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      a = data.map((item) => {
+        return `
+        <div class="list_container">
+        <li>${item.id}</li>
+        <li>${item.title}</li>
+        <li>${item.body}</li>
+        </div>
+`;
+      });
+      addFetch.innerHTML = a;
     });
-    fech.innerHTML = a;
-  });
+}
+
+function render() {
+  addFetch.innerHTML = "";
+}
+
+btn.addEventListener("click", addText);
+
+backBtn.addEventListener("click", render);
